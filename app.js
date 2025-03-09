1887f12e797e6c976cfbe50e000380f066a8612b
shivakumar <shivamahajan055@gmail.com>
2025-03-09 13:28:50 +0530

    added routes.js

--- app.js
+++ app.js
@@ -12,48 +12,10 @@ used in video streaming and large size file uploads
 */
 
 const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require("./routes");
+const server = http.createServer(routes);
 
-  if (req.url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      `<form action="/message" method="POST">
-        <label id="username">Name:</label>
-        <input type="text" name="username" />
-        <button type="submit">Add</button>
-      </form>
-      `
-    );
-  } else {
-    if (req.url == "/message") {
-      res.setHeader("Content-Type", "text/html");
-      let dataChunks = [];
-      req.on("data", (chunks) => {
-        dataChunks.push(chunks);
-      });
-
-      req.on("end", () => {
-        let combinedBuffer = Buffer.concat(dataChunks);
-        let formData = combinedBuffer.toString().split("=")[1];
-        fs.writeFile("formValues.txt", formData, (err) => {
-          res.statusCode = 302;
-          res.setHeader("Location", "/");
-          res.end();
-        });
-      });
-    } else {
-      if (req.url == "/read") {
-        //read from file
-        fs.readFile("formValues.txt", (err, data) => {
-          res.end(`<h1>${data.toString()}</h1>`);
-        });
-      }
-    }
-  }
-});
+routes.testFunction(2, 3);
 
 server.listen(3000, () => {
   console.log("server is running!");
---
+++ routes.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      `<form action="/message" method="POST">
+              <label id="username">Name:</label>
+              <input type="text" name="username" />
+              <button type="submit">Add</button>
+            </form>
+            `
+    );
+  } else {
+    if (req.url == "/message") {
+      res.setHeader("Content-Type", "text/html");
+      let dataChunks = [];
+      req.on("data", (chunks) => {
+        dataChunks.push(chunks);
+      });
+
+      req.on("end", () => {
+        let combinedBuffer = Buffer.concat(dataChunks);
+        let formData = combinedBuffer.toString().split("=")[1];
+        fs.writeFile("formValues.txt", formData, (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        });
+      });
+    } else {
+      if (req.url == "/read") {
+        //read from file
+        fs.readFile("formValues.txt", (err, data) => {
+          res.end(`<h1>${data.toString()}</h1>`);
+        });
+      }
+    }
+  }
+};
+
+const sum = (a, b) => {
+  console.log(a + b);
+};
+
+// module.exports = {
+//   requestHandler,
+//   sum,
+// };
+
+// module.exports = {
+//  handler :  requestHandler,
+//  calSum :  sum,
+// };
+
+//direct export keyword
+
+module.exports.handler = requestHandler;
+module.exports.testFunction = sum;
